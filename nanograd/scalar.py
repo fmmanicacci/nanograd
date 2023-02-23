e2b073925dccf0ee18ea7f889d5f6c46f58d8674
Manicacci François-Marie <manicacci.fm@gmail.com>
2023-02-23 19:43:26 +0100

    Updated the Scalar object and its __str__ method

--- nanograd/scalar.py
+++ nanograd/scalar.py
@@ -1,6 +1,6 @@
 """Definition of the Scalar object."""
 
-from .enums import Operator
+from .enums import Operation
 
 class Scalar:
     """
@@ -13,12 +13,14 @@ class Scalar:
         self,
         data: int | float,
         label: str | None = None,
+        requires_grad: bool = False,
         _prev: set['Scalar'] = None,
-        _op: Operator = Operator.NONE
+        _op: Operation = Operation.NONE
     ) -> None:
         """Constructor."""
         self.data = data
         self.label = label
+        self.requires_grad = requires_grad
         self._grad = 0.0
         self._prev = set() if _prev is None else _prev
         self._op = _op
@@ -28,9 +30,9 @@ class Scalar:
         """Provide a string representation of the object."""
         label_str = "" if self.label is None else f"label={self.label}, "
         data_str = f"data={self.data:.6f}"
-        grad_str = "" if self._op is Operator.NONE else f", grad={self._grad:.6f}"
-        op_str = "" if self._op is Operator.NONE else f", op={self._op.value}"
-        prev_str = "" if self._op is Operator.NONE else f", prev={len(self._prev)}"
+        grad_str = "" if not self.requires_grad else f", grad={self._grad:.6f}"
+        op_str = "" if self._op is Operation.NONE else f", op={self._op.value}"
+        prev_str = "" if self._op is Operation.NONE else f", prev={len(self._prev)}"
         return f"Value({label_str}{data_str}{grad_str}{op_str}{prev_str})"
         
     def __repr__(self) -> str:
@@ -39,5 +41,5 @@ class Scalar:
 
 
 if __name__ == '__main__':
-    print(Scalar(42, _op=Operator.ADD))
+    print(Scalar(42, _op=Operation.ADD))
     
--- tests/test_scalar.py
+++ tests/test_scalar.py
@@ -1,27 +1,38 @@
 """Test suit for the Scalar object."""
 
 from nanograd.scalar import Scalar
-from nanograd.enums import Operator
+from nanograd.enums import Operation
 
 def test_str_no_label_op_none() -> None:
-    str_repr = Scalar(42).__str__()
-    exp_repr = "Value(data=42.000000)"
+    a = Scalar(21)
+    str_repr = a.__str__()
+    exp_repr = "Value(data=21.000000)"
     assert str_repr == exp_repr
 
 
 def test_str_with_label_op_none() -> None:
-    str_repr = Scalar(42, label="a").__str__()
-    exp_repr = "Value(label=a, data=42.000000)"
+    a = Scalar(21, label="a")
+    str_repr = a.__str__()
+    exp_repr = "Value(label=a, data=21.000000)"
     assert str_repr == exp_repr
 
 
-def test_str_no_label_op_add() -> None:
-    str_repr = Scalar(42, _op=Operator.ADD).__str__()
-    exp_repr = "Value(data=42.000000, grad=0.000000, op=add, prev=0)"
+def test_str_no_label_op_identity() -> None:
+    a = Scalar(21, _op=Operation.IDENTITY, _prev={Scalar(21),})
+    str_repr = a.__str__()
+    exp_repr = "Value(data=21.000000, op=identity, prev=1)"
     assert str_repr == exp_repr
 
   
-def test_str_with_label_op_add() -> None:
-    str_repr = Scalar(42, label="a", _op=Operator.ADD).__str__()
-    exp_repr = "Value(label=a, data=42.000000, grad=0.000000, op=add, prev=0)"
+def test_str_with_label_op_identity() -> None:
+    a = Scalar(21, label="a", _op=Operation.IDENTITY, _prev={Scalar(21),})
+    str_repr = a.__str__()
+    exp_repr = "Value(label=a, data=21.000000, op=identity, prev=1)"
+    assert str_repr == exp_repr
+
+
+def test_str_with_label_op_identity_requires_grad() -> None:
+    a = Scalar(21, label="a", requires_grad=True, _op=Operation.IDENTITY, _prev={Scalar(21),})
+    str_repr = a.__str__()
+    exp_repr = "Value(label=a, data=21.000000, grad=0.000000, op=identity, prev=1)"
     assert str_repr == exp_repr
