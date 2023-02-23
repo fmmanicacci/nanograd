6b3e3b6383faef856a254b452918fa2ff28354a5
François-Marie Manicacci <francois-marie.manicacci_ext@michelin.com>
2023-02-23 14:04:00 +0100

    Setup the project's dependencies

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='nanograd',
+    packages=find_packages(include=['nanograd']),
+    version='0.1.0',
+    description='A tiny scalar-valued autograd engine inspired from the one created by Karpathy',
+    author='François-Marie Manicacci',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==7.2.1'],
+    test_suite='tests',
+)
