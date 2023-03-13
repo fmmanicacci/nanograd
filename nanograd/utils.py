ed3c3133444041694bd97b040b792080c6b2f9fb
Manicacci François-Marie <manicacci.fm@gmail.com>
2023-03-13 13:24:34 +0100

    Removal of typing to eliminate workaround for circular import

--- nanograd/utils.py
+++ nanograd/utils.py
@@ -1,15 +1,11 @@
 """Module containing utility functions for nanograd."""
 
-from typing import TYPE_CHECKING
 from ordered_set import OrderedSet
 
-if TYPE_CHECKING:
-    from .scalar import Scalar
-
-def topological_sort(root: 'Scalar') -> OrderedSet['Scalar']:
+def topological_sort(root) -> OrderedSet:
         """Topological sort of the computational graph."""
         topo, visited = OrderedSet(), set()
-        def _topological_sort(node: 'Scalar') -> None:
+        def _topological_sort(node) -> None:
             if node not in visited:
                 visited.add(node)
                 for prev in node._prev:
